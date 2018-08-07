853a623e5579ef4f8996228b63a3eedfe07e6600
Jason Wang <jason.wang592@gmail.com>
2018-08-06 23:43:24 -0700

    Refactor script. Age/gender binning cleaning

--- Ford_GoBike/analysis.py
+++ Ford_GoBike/analysis.py
@@ -8,15 +8,14 @@ import plotting
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import seaborn as sns
-
-
+import numpy as np
 plt.style.use('fivethirtyeight')
+
+#Setting some high level variables
 data_dir = 'data/'
 pickle_name = 'GoBike.p'
 output_dir = 'output/'
-save_run = True
 ordered_days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-
 sf_long_coord = [-122.517815,-122.380314]
 east_long_coord = [-122.318687,-122.213287]
 
@@ -52,17 +51,17 @@ def pickler(repickle=False):
 
     df['route'] = df['start_station_name'].map(str) + '-' + df['end_station_name'].map(str)
     df['start_time'] = pd.to_datetime(df['start_time'])
+    df['start_hour'] = df['start_time'].dt.hour
     df['year'] = df['start_time'].dt.year
     df['month_num'] = df['start_time'].dt.month
     df['month'] = df['month_num'].apply(lambda x: calendar.month_abbr[x])
     df['month_year'] = df['month'].map(str) + ' ' + df['year'].map(str)
     df['day_of_week'] = df['start_time'].dt.weekday_name
 
-    pickle.dump(df, open(pickle_name, 'wb'))
-  else:
-    return pickle.load(open(pickle_name, 'rb'))
+    pickle.dump(df,open(pickle_name, 'wb'))
+  return pickle.load(open(pickle_name, 'rb'))
 
-def weekday_analysis(df, top_n, output_dir, save_run):
+def weekday_net_bars(df, top_n, output_dir, save):
   #Now get top N stations by average deficit/surplus by day of week
   weekday_df = df.sort_values(['day','net_bikes']).groupby(['day','start_station_name'])['start_station_name','net_bikes'].mean()
   top_deficit = weekday_df.groupby('day').apply(lambda x: x.nsmallest(top_n, 'net_bikes',keep='first')).reset_index(level=0,drop=True)
@@ -80,7 +79,8 @@ def weekday_analysis(df, top_n, output_dir, save_run):
       ylab='Station Name',
       day=str(day),
       output_dir=output_dir+'Net Bikes by Weekday/'+'Total/',
-      save=save_run)
+      save=save,
+      orientation='h')
 
   #Do it again but split by user type:
   for user in list(df['user_type'].unique()):
@@ -101,81 +101,155 @@ def weekday_analysis(df, top_n, output_dir, save_run):
         ylab='Station Name',
         day=str(day),
         output_dir=output_dir+'Net Bikes by Weekday/'+user+'/',
-        save=save_run)
+        save=save,
+        orientation='h')
 
+def hour_sum_bars(df, stations, output_dir, save, split_user=False):
 
-'''Main script'''
-df = pickler()
-cur_region = 'San Francisco'
-if not os.path.exists(output_dir):
-  os.mkdir(output_dir)
-stations = list(df['start_station_name'].unique())
-df['date'] = df['start_time'].dt.date
-df['day_of_week'] = pd.Categorical(df['day_of_week'], ordered_days)
-df = df[df['region'] == cur_region]
-output_dir += cur_region + '/'
+  for station in stations:
+    o_dir = output_dir
+    station_df = df[df['start_station_name']==station]
+    if split_user:
+      for user in station_df['user_type'].unique():
+        user_df = station_df[station_df['user_type']==user]
+        hour_df = user_df.groupby('start_hour')['bike_id'].count().reset_index(name='rides')
+        plotting.bar_wrapper(df=hour_df,
+          x='start_hour',
+          y='rides',
+          title= ' - '.join(['Rides by hour',station]),
+          xlab='Hour',
+          ylab='Number of Rides',
+          output_dir=output_dir+ user +'/',
+          save=save,
+          suffix=station,
+          orientation='v')
+    else:
+      o_dir = output_dir + 'All Users/'
+      hour_df = station_df.groupby('start_hour')['bike_id'].count().reset_index(name='rides')
+      print(station, hour_df)
+      plotting.bar_wrapper(df=hour_df,
+        x='start_hour',
+        y='rides',
+        title= ' - '.join(['Rides by hour',station]),
+        xlab='Hour',
+        ylab='Number of Rides',
+        output_dir=output_dir+'Total/',
+        save=save,
+        suffix=station,
+        orientation='v')
 
-'''
-Create two aggregated DataFrames that each groups by date and station (pick up and drop off)
-then inner join on station and calculate the net bikes at each station for every date to observe
-where the surplus and deficit in bikes are.
-'''
-
-#Aggregate all bike sessions by date and station to get a count
-start_df = df.groupby(['date','start_station_name','user_type']).size().reset_index()
-end_df = df.groupby(['date', 'end_station_name','user_type']).size().reset_index()
-start_df.columns = ['date','start_station_name','user_type','picked_up']
-end_df.columns = ['date','end_station_name','user_type', 'dropped_off']
-start_df.set_index('start_station_name')
-end_df.set_index('end_station_name')
-
-#Tie the dataframes together and get the net flow of bikes for each station
-joint_df = pd.concat([start_df,end_df],axis=1,join='inner')
-joint_df['net_bikes'] = joint_df['dropped_off'] - joint_df['picked_up']
-joint_df.drop(['dropped_off','picked_up'],axis=1,inplace=True)
-#Drop duplicate columns
-_,i = np.unique(joint_df.columns,return_index=True)
-joint_df = joint_df.iloc[:,i]
-joint_df['day'] = pd.to_datetime(joint_df['date']).dt.weekday_name
-joint_df['day'] = pd.Categorical(joint_df['day'], ordered_days)
-
-'''Here we'll understand how bikes move throughout the city and region'''
-top_n = 5
-
-#Get the stations that have the greatest surplus and deficit in general
-grouped = joint_df.groupby('start_station_name').mean().sort_values('net_bikes')
-top_surplus = grouped.head(top_n)
-top_deficit = grouped.tail(top_n)
-
-# weekday_analysis(joint_df, top_n, output_dir, save_run)
-
-'''Get the top N stations by date that have the most surplus or deficit of bikes'''
-sorted_joint_df = joint_df.sort_values(['date','net_bikes']).groupby('date')['start_station_name', 'net_bikes']
-top_deficit = sorted_joint_df.apply(lambda x: x.nsmallest(top_n,'net_bikes'))
-top_surplus = sorted_joint_df.apply(lambda x: x.nlargest(top_n, 'net_bikes'))
-
-'''Let's find some aggregate numbers about ridership by date, day of week, user type and so on'''
-df.index = df['start_time']
-grouped = df.groupby(['year','month_num','user_type'], as_index = False)['bike_id'].count().rename(columns = {'bike_id':'count'})
-grouped['date'] = pd.to_datetime(grouped['year'].astype(str) + grouped['month_num'].astype(str), format='%Y%m')
-grouped['formatted_date'] = grouped['month_num'].apply(lambda x: calendar.month_abbr[x]) + ' ' + grouped['year'].map(str)
-grouped = grouped.set_index('date')
-
-fig = plt.figure(figsize = (15,10))
-ax1 = grouped[grouped['user_type']=='Subscriber']['count'].plot(x_compat=True)
-ax2 = grouped[grouped['user_type']=='Customer']['count'].plot(x_compat=True)
-ax1.xaxis.set_major_locator(mdates.MonthLocator())
-title = 'Ridership by User Type'
-plt.title(title)
-plt.xlabel('Date')
-plt.ylabel('Rides')
-fname = 'Ridership by User Type'
-if save_run:
+def station_analysis(df, region, save_run):
+  '''
+  Performs analysis on stations based on ridership for dates and net bikes at stations for weekdays
+  Args:
+    - df        (DataFrame): The DataFrame from the pickled object
+    - region    (String)   : The region to analyze (San Francisco, East Bay, South Bay)
+    - save_run  (Boolean)  : If true, saves all charts, otherwise displays them
+  '''
+  df = pickler()
   if not os.path.exists(output_dir):
-    os.makedirs(output_dir)
-  plt.savefig(output_dir + fname)
-else:
-  plt.show()
+    os.mkdir(output_dir)
+  stations = list(df['start_station_name'].unique())
+  df['date'] = df['start_time'].dt.date
+  df['day_of_week'] = pd.Categorical(df['day_of_week'], ordered_days)
+  df = df[df['region'] == region]
+  output_dir += cur_region + '/'
+
+  '''
+  Create two aggregated DataFrames that each groups by date and station (pick up and drop off)
+  then inner join on station and calculate the net bikes at each station for every date to observe
+  where the surplus and deficit in bikes are.
+  '''
+
+  #Aggregate all bike sessions by date and station to get a count
+  start_df = df.groupby(['date','start_station_name','user_type']).size().reset_index()
+  end_df = df.groupby(['date', 'end_station_name','user_type']).size().reset_index()
+  start_df.columns = ['date','start_station_name','user_type','picked_up']
+  end_df.columns = ['date','end_station_name','user_type', 'dropped_off']
+  start_df.set_index('start_station_name')
+  end_df.set_index('end_station_name')
+
+  #Tie the dataframes together and get the net flow of bikes for each station
+  joint_df = pd.concat([start_df,end_df],axis=1,join='inner')
+  joint_df['net_bikes'] = joint_df['dropped_off'] - joint_df['picked_up']
+  joint_df.drop(['dropped_off','picked_up'],axis=1,inplace=True)
+  #Drop duplicate columns
+  _,i = np.unique(joint_df.columns,return_index=True)
+  joint_df = joint_df.iloc[:,i]
+  joint_df['day'] = pd.to_datetime(joint_df['date']).dt.weekday_name
+  joint_df['day'] = pd.Categorical(joint_df['day'], ordered_days)
+
+  '''Here we'll understand how bikes move throughout the city and region'''
+  top_n = 5
+
+  #Get the stations that have the greatest surplus and deficit in general
+  grouped = joint_df.groupby('start_station_name').mean().sort_values('net_bikes')
+  top_surplus = grouped.head(top_n)
+  top_deficit = grouped.tail(top_n)
+
+  # Plotting out, by weekday, what stations have the most surplus or deficit
+  weekday_analysis(joint_df, top_n, output_dir, save_run)
+
+  # For the top_n stations by ridership, plots the ridership for those stations by hour
+  top_n = 10
+  temp = df.groupby(['start_station_name'])['bike_id'].count().reset_index(name='total_rides').sort_values('total_rides', ascending=False)
+  station_list = list(temp.head(top_n)['start_station_name'])
+  hour_sum_bars(df,station_list,output_dir=output_dir+'Rides by hour/',save=save_run, split_user=True)
+
+  '''Get the top N stations by date that have the most surplus or deficit of bikes'''
+  sorted_joint_df = joint_df.sort_values(['date','net_bikes']).groupby('date')['start_station_name', 'net_bikes']
+  top_deficit = sorted_joint_df.apply(lambda x: x.nsmallest(top_n,'net_bikes'))
+  top_surplus = sorted_joint_df.apply(lambda x: x.nlargest(top_n, 'net_bikes'))
+
+
+if __name__ == '__main__':
+  save_run = True
+  df = pickler()
+  # station_analysis(df, 'San Francisco', save_run)
+
+  #Of 1,338,864 rides, roughly 125,000 are missing data on birth_year and gender. We'll drop those
+  numerical_df = df.copy()
+  numerical_df = numerical_df.dropna(subset=['birth_year','gender'])
+  numerical_df['age'] = numerical_df['birth_year'].apply(lambda x: pd.Timestamp.now().year - x)
+  numerical_df['age'] = numerical_df['age'].map(int)
+  bins = [18,23,28,33,38,43,48,53,58,63,68,73,78,83,88,93,98,np.inf]
+  bin_names = ['18-23','23-28','28-33','33-38','38-43','43-48','48-53','53-58','58-63','63-68',
+              '68-73','73-78','78-83','83-88','88-93','93-98','98+']
+  d = dict(enumerate(bin_names,1))
+  numerical_df['age_range'] = list(map(d.get, np.digitize(numerical_df['age'],bins)))
+  print(numerical_df.head())
+  sys.exit()
+  print('Average age for all riders: ' + str(numerical_df['age'].mean()))
+  average_gender = numerical_df.groupby('gender')['age'].mean().plot()
+  gender_count = numerical_df.groupby('gender').size()
+  user_gender_count = numerical_df.groupby(['user_type','gender']).count()
+  ageRange_count = numerical_df.groupby('age_range').count()
+  user_ageRange_count = numerical_df.groupby(['user_type','age_range']).count()
+  print(count_gender)
+  sys.exit()
+
+  '''Let's find some aggregate numbers about ridership by date, day of week, user type and so on'''
+  df.index = df['start_time']
+  grouped = df.groupby(['year','month_num','user_type'], as_index = False)['bike_id'].count().rename(columns = {'bike_id':'count'})
+  grouped['date'] = pd.to_datetime(grouped['year'].astype(str) + grouped['month_num'].astype(str), format='%Y%m')
+  grouped['formatted_date'] = grouped['month_num'].apply(lambda x: calendar.month_abbr[x]) + ' ' + grouped['year'].map(str)
+  grouped = grouped.set_index('date')
+
+  fig = plt.figure(figsize = (15,10))
+  ax1 = grouped[grouped['user_type']=='Subscriber']['count'].plot(x_compat=True)
+  ax2 = grouped[grouped['user_type']=='Customer']['count'].plot(x_compat=True)
+  ax1.xaxis.set_major_locator(mdates.MonthLocator())
+  title = 'Ridership by User Type and Month'
+  plt.title(title)
+  plt.xlabel('Date')
+  plt.ylabel('Rides')
+  fname = 'Ridership by User Type and Month'
+  if save_run:
+    if not os.path.exists(output_dir):
+      os.makedirs(output_dir)
+    plt.savefig(output_dir + fname)
+  else:
+    plt.show()
 
 
 '''
--- Ford_GoBike/plotting.py
+++ Ford_GoBike/plotting.py
@@ -1,26 +1,38 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
 import os
+import numpy as np
 
-def bar_wrapper(df,x,y,title,xlab,ylab,output_dir,save,day=None):
+def bar_wrapper(df,x,y,title,xlab,ylab,output_dir,save,suffix,orientation='v'):
   '''Plots the barchart for corresponding to some value associated with a name
-
   Args:
-    - df        (DataFrame): The dataframe containing all data
-    - gender    (String)   : What gender data is contained within the dataframe
-    - output_dir(String)   : The directory to save output to
-    - save      (Boolean)  : Saves the file by default, if set to False, displays the plot instead
+    - df          (DataFrame): The dataframe containing all data
+    - x           (String)   : Name of dataframe column to act as independent variable if vertical
+    - y           (String)   : Name of dataframe column to act as dependent variable if vertical
+    - title       (String)   : Title of the chart
+    - xlab        (String)   : X-axis label
+    - ylab        (String)   : Y-axis label
+    - output_dir  (String)   : Base directory for this set of charts
+    - save        (Boolean)  : Saves the file by default, if set to False, displays the plot instead
+    - suffix      (String)   : Suffix to append to title, also the filename
+    - orientation (String)   : 'v' for vertical bar chart, 'h' for horizontal bars
   '''
+
   fig = plt.figure(figsize = (15,9))
-  fname =' - '.join([title,day])
-  sns.barplot(data=df, x=x, y=y,palette=sns.color_palette("RdYlGn_r", 10))
+  if orientation == 'v':
+    pal = sns.color_palette("RdYlGn_r", len(df[x].unique()))
+    rank = df[y].argsort().argsort()
+  else:
+    pal = sns.color_palette("RdYlGn_r", len(df[y].unique()))
+    rank = df[x].argsort().argsort()
+  sns.barplot(data=df, x=x, y=y,palette=np.array(pal[::-1])[rank])
   plt.xlabel(xlab)
   plt.ylabel(ylab)
-  plt.title(fname)
+  plt.title(title)
   plt.yticks(fontsize=10)
 
   plt.tight_layout()
-  fname ='-'.join([title,day])
+  fname = suffix
 
   if save:
     if not os.path.exists(output_dir):
